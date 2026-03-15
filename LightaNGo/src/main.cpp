eb3074c1d38b2289c6523f1acfec4ea1ff4bec97
Jayesh-l <2403031307008@paruluniversity.ac.in>
2026-03-15 21:24:01 +0530

    mid presentation push and commit

--- LightaNGo/src/main.cpp
+++ LightaNGo/src/main.cpp
@@ -1,130 +1,46 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <optional>
 #include <sstream>
 #include <vector>
-    
-using namespace std;
 
-enum class TokenType
-{
-    retreat,
-    int_literal,
-    semicolon
-};
+#include "generation.hpp"
 
-struct Token
+int main(int argc, char* argv[])
 {
-    TokenType type;
-    optional<string> value {};
-};
-string tokens_to_asm(const vector<Token>& tokens)
-{
-    stringstream output;
-    output << " global _start\n_start:\n";
-    for (int i = 0;i<tokens.size();i++)
-    {
-        auto token = tokens[i];
-        if (token.type == TokenType::retreat)
-        {
-            if (i+1<tokens.size() && tokens[i+1].type == TokenType::int_literal)
-            {
-                if (i+2<tokens.size() && tokens[i+2].type == TokenType::semicolon)
-                {
-                    output<<"   mov rax,60\n";
-                    output<<"   mov rdi, "<<tokens[i+1].value.value()<<"\n";
-                    output<<"   syscall";
-                }
-            }
-        }
-    }
-    return output.str();
-
-}
-vector<Token> tokenize(const string &str)
-{
-    vector<Token> tokens;
-    string buf;
-    for (int i=0;i<str.length();i++)
-    {
-        char c = str[i];
-        if (isalpha(c))
-        {
-            buf.push_back(c);
-            i++;
-            while (i<str.size() && isalpha(str[i]))
-            {
-                buf.push_back(str[i]);
-                i++;
-            }
-            i--;
-            if (buf == "retreat")
-            {
-                // Token t;
-                // t.type = TokenType::retreat;
-                // t.value = std::nullopt;
-                // tokens.push_back(t);
-
-                tokens.push_back({.type = TokenType::retreat});
-                buf.clear();
-                continue;
-            }
-            else
-            {
-                cerr<<"Kar diya kaand"<<endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (isdigit(c))
-        {
-            buf.push_back(c);
-            i++;
-            while (i<str.size() && isdigit(str[i]))
-            {
-                buf.push_back(str[i]);
-                i++;
-            }
-            i--;
-            tokens.push_back({.type = TokenType::int_literal,.value = buf});
-            buf.clear();
-        }
-        else if (c == ';')
-        {
-            tokens.push_back({.type = TokenType::semicolon});
-        }
-        else if (isspace(c))
-        {
-            continue;
-        }
-        else
-        {
-            cout<<"kar diya kaand";
-        }
-    }
-    return tokens;
-}
-
-int main(int argc,char* argv[])
-{
-    if (argc != 2)
-    {
-        cerr<<"Incorrectly done. Do it like..."<<endl;
-        cerr<<"ltn <input.lango>"<<endl;
+    if (argc != 2) {
+        std::cerr<<"Incorrectly done. Do it like..."<<std::endl;
+        std::cerr<<"ltn <input.lango>"<<std::endl;
         return EXIT_FAILURE;
     }
-    string contents;
+
+    std::string contents;
     {
-        stringstream contents_stream;
-        fstream input(argv[1],ios::in);
-        contents_stream<<input.rdbuf();
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
         contents = contents_stream.str();
     }
-    {
-        vector <Token> tokens = tokenize(contents);
-        fstream file("out.asm",ios::out);
-        file<<tokens_to_asm(tokens);
+
+    Tokenizer tokenizer(std::move(contents));
+    std::vector<Token> tokens = tokenizer.tokenize();
+
+    Parser parser(std::move(tokens));
+    std::optional<NodeProg> prog = parser.parse_prog();
+
+    if (!prog.has_value()) {
+        std::cerr << "Invalid program" << std::endl;
+        exit(EXIT_FAILURE);
     }
+
+    {
+        Generator generator(prog.value());
+        std::fstream file("out.asm", std::ios::out);
+        file << generator.gen_prog();
+    }
+
     system("nasm -felf64 out.asm");
     system("ld -o out out.o");
+
     return EXIT_SUCCESS;
 }
